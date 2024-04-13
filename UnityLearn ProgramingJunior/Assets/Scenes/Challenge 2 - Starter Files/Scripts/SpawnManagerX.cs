d8c3d1e90d22824d8d6282f26db29f74526fecc4
Guvunsu <gavryk_fray_rojas@hotmail.com>
2024-04-13 17:35:55 -0600

    LESSION 1 COMPLETADA

--- UnityLearn ProgramingJunior/Assets/Scenes/Challenge 2 - Starter Files/Scripts/SpawnManagerX.cs
+++ UnityLearn ProgramingJunior/Assets/Scenes/Challenge 2 - Starter Files/Scripts/SpawnManagerX.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class SpawnManagerX : MonoBehaviour
 {
     public GameObject[] ballPrefabs;
-    public int balls
+    public int balls;//ahi me quede la ultima vez en este challenger
+
 
     private float spawnLimitXLeft = -22;
     private float spawnLimitXRight = 7;
