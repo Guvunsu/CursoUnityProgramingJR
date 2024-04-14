a501223c014c201c5f82af45b370a67e804f355c
Guvunsu <gavryk_fray_rojas@hotmail.com>
2024-04-14 08:46:29 -0600

    se cayo unity web

--- UnityLearn ProgramingJunior/Assets/Scenes/Prototype 4/SCRIPTS/MovPlayerPrototype4.cs
+++ UnityLearn ProgramingJunior/Assets/Scenes/Prototype 4/SCRIPTS/MovPlayerPrototype4.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class MovPlayerPrototype4 : MonoBehaviour {
     public Rigidbody body;
     private GameObject focalPoint;
+    Vector3 awayFromPlayer;
+
+
     public float speed = 5;
+    public bool hasPowerUp;
+    private float powerupStrength = 15.0f;
+
     float inVe;
     void Start() {
         body = GetComponent<Rigidbody>();
@@ -19,5 +25,23 @@ public class MovPlayerPrototype4 : MonoBehaviour {
     void MovePlayerPr4() {
         inVe = Input.GetAxis("Vertical");
         body.AddForce(focalPoint.transform.forward * inVe * speed);
+    }
+    private void OnTriggerEnter(Collider power) {
+        if (power.CompareTag("PowerUp")) {
+            Destroy(power.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+    IEnumerator PowerupCountdownRoutine() {
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
+    }
+    private void OnCollisionEnter(Collision collisions) {
+        if (collisions.gameObject.CompareTag("Enemy") && hasPowerUp) {
+            body = collisions.gameObject.GetComponent<Rigidbody>();
+            awayFromPlayer = (collisions.gameObject.transform.position - transform.position);
+            Debug.Log("Collided with" + collisions.gameObject.name + "with powerup set to" + hasPowerUp);
+            body.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
     }
 }
