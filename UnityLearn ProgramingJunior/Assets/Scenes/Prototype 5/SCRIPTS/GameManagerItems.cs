cfad6311d6d22fb3e4e5b26dbc316f53e9f3731a
Guvunsu <gavryk_fray_rojas@hotmail.com>
2024-04-15 04:57:43 -0600

    lession 5 , tienee muchos errores inexplicables a lo mejro por la ver.UNITY que use , no se , pedire ayuda a algun maestro

--- UnityLearn ProgramingJunior/Assets/Scenes/Prototype 5/SCRIPTS/GameManagerItems.cs
+++ UnityLearn ProgramingJunior/Assets/Scenes/Prototype 5/SCRIPTS/GameManagerItems.cs
@@ -2,34 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManagerItems : MonoBehaviour {
+
+
+    public TextMeshProUGUI gameOverText;
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
-    private int score;
-    int index;
-    private float spawnRate = 1.0f;
-    void Start() {
-        StartCoroutine(SpawnTargetz());
-        UpdateScore(0);
-        score = 0;
-        scoreText.text = "Score" + score;
-    }
+    public Button restartButton;
+    public GameObject titleScreen;//no me quiere sallir para el gameobject game manager
 
-    // Update is called once per frame
+
+    int index;
+
+    private int score;
+    private float spawnRate = 1.0f;
+
+    public bool isGameActive;
+
+    void Start() {
+        StartGame(difficulty);//corregir esto
+    }
     void Update() {
 
     }
 
+    public void StartGame(int difficulty) {
+        isGameActive = true;
+        score = 0;
+        spawnRate /= difficulty;
+        StartCoroutine(SpawnTargetz());
+        UpdateScore(0);
+        gameOverTextMeshPro();
+    }
     private void UpdateScore(int scoreToAdd) {
         score += scoreToAdd;
         scoreText.text = "Score" + score;
     }
+
     IEnumerator SpawnTargetz() {
-        while (true) {
+        while (isGameActive) {
             yield return new WaitForSeconds(spawnRate);
             index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+    }
+
+    public void gameOverTextMeshPro() {
+
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    void RestartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        titleScreen.gameObject.SetActive(false);
     }
 }
